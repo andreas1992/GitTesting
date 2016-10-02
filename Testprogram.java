fbbc8c1d4f678aa63d497c40adace24136101cc4
Andreas <aks1992@gmail.com>
2016-10-02 19:56:14 +0200

    Initial commit, made first class

---
+++ Testprogram.java
@@ -0,0 +1,14 @@
+
+public class Testprogram {
+    
+    private String name;
+    
+    public Testprogram(String name) {
+        this.name = name;
+    }
+    
+    public void printWelcome() {
+        System.out.println("Hello and welcome " + name + "!");
+    }
+    
+}
