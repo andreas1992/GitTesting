cc370e92400e94b97a88d723f3acedf343414a96
Andreas <aks1992@gmail.com>
2016-10-02 20:04:35 +0200

    Makes java .class files (compiled files)

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     
-    public static void main (String args[]) {
+    public static void main (String args[]){
         Testprogram testP = new Testprogram("Andreas");
         testP.printWelcome();
     }
