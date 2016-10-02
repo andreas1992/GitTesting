d921830c1fd2dfb49ba6d807dcfbae54cd5546f9
Andreas <aks1992@gmail.com>
2016-10-02 21:57:49 +0200

    Legger til kommentar

--- Testprogram.java
+++ Testprogram.java
@@ -3,6 +3,9 @@ public class Testprogram {
     private String name;
     private String lastname;
     
+	/**
+	* Constructor som starter klassen.
+	*/
     public Testprogram(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
